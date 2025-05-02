28de2fd4dc798d067fe286d784e60d326665365b
GovindaRadha <newuser@example.com>
2025-05-02 22:23:25 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   // Removing output: 'export' and distDir for dynamic deployment
   typescript: {
     ignoreBuildErrors: true,
